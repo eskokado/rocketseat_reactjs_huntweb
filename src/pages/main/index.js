2ad3e17e75ec39bd9a388d33569a8308f46c0ad4
Edson Shideki Kokado <eskokado@gmail.com>
2019-10-17 18:09:15 -0300

    Armazenando no estado

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 class Main extends Component {
+    state = {
+        products: [],
+    }
 
     componentDidMount() {
         this.loadProducts();
@@ -10,11 +13,17 @@ class Main extends Component {
     loadProducts = async () => {
         const response = await api.get('/products');
 
-        console.log(response.data.docs);
+        this.setState({ products: response.data.docs });
     };
 
     render() {
-        return <h1>Hello Rocketseat</h1>;
+        return (
+            <div className='product-list'>
+                {this.state.products.map(product => (
+                    <h2 key={product._id}>{product.title}</h2>
+                ))}
+            </div>
+        );
     }
 }
 
