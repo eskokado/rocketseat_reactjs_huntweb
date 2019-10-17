8d46373e1c0f025912c374feebf053c18dc63e10
Edson Shideki Kokado <eskokado@gmail.com>
2019-10-17 15:36:36 -0300

    Criando a Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello Esk Info Tech Web</h1>  
-      </div>
-    );
-  }
-}
+import './styles.css';
+
+import Header from './components/Header';
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello Esk Info Tech Web</h1>  
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">JSHunt</header>
+);
+
+export default Header;
+
+
+// const Header = () => (
+    
+// );
+// equivalente
+// class Header extends Component {
+//     render() {
+//         return <h1>Hello</h1>
+//     }
+// }
